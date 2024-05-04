95e406d4ad619fc0eef99629c70629f2c406eb3f
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-05-04 01:39:58 -0400

    fix: changed mongoose models

--- src/db/models/hackathon.ts
+++ src/db/models/hackathon.ts
@@ -5,8 +5,8 @@ const HackathonSchema = new Schema({
   location: { type: String, required: true },
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
-  organizers: [{ type: Schema.Types.ObjectId, ref: "Organizer" }],
-  hackers: [{ type: Schema.Types.ObjectId, ref: "Hacker" }],
+  organizers: [{ type: String, ref: "Organizer" }],
+  hackers: [{ type: String, ref: "Hacker" }],
   events: [{
     name: { type: String, required: true },
     startDate: { type: Date, required: true },

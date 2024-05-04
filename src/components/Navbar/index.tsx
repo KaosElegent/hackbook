50d78489c0c0ac6c7c91e17ee90bda5727bd671a
the creator <chenxi.yan@gmail.com>
2024-05-04 01:36:46 -0400

    created navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from "react";
+import {
+  Navbar as NextUINavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownTrigger,
+  User,
+  DropdownMenu
+} from "@nextui-org/react";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const menuItems = [
+    "Home",
+    "Profile",
+    "Log Out",
+  ];
+
+  const { user } = useUser();
+
   return (
     <div>
-      
+      <NextUINavbar onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <p className="font-bold text-inherit">HackBook</p>
+          </NavbarBrand>
+        </NavbarContent>
+
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem isActive>
+            <Link color="foreground" href="#">
+              Home
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">Dashboard</Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            {user ? (
+              <Dropdown placement="bottom-start">
+                <DropdownTrigger>
+                  <User
+                    as="button"
+                    avatarProps={{
+                      isBordered: true,
+                      src: user.picture || undefined,
+                    }}
+                    className="transition-transform"
+                    description={`@${user.nickname}`}
+                    name={user.name}
+                  />
+                </DropdownTrigger>
+                <DropdownMenu aria-label="User Actions" variant="flat">
+                  <DropdownItem key="profile" className="h-14 gap-2">
+                    <p className="font-bold">Signed in as</p>
+                    <p className="font-bold">@{user.nickname}</p>
+                  </DropdownItem>
+                  <DropdownItem key="logout">
+                    <Link href="/api/auth/logout">
+                      <Button color="danger">Log Out</Button>
+                    </Link>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            ) : (
+              <Link href="/api/auth/login">
+                <Button className="dark">Sign In</Button>
+              </Link>
+            )}
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu>
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === menuItems.length - 1
+                    ? "danger"
+                    : "foreground"
+                }
+                className="w-full"
+                href="#"
+              >
+                {item}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </NextUINavbar>
     </div>
-  )
+  );
 }

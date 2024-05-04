9e5e72a52b47e6dc9a623591a19b7da1c6ef838b
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-05-04 00:21:42 -0400

    Inital Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import {NextUIProvider} from '@nextui-org/react'
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <NextUIProvider>
+      <main>
+      </main>
+    </NextUIProvider>
+  );
+}

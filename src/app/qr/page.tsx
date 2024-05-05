29adb62030392075a76ff313149dc3fd053f9576
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-05-05 01:13:54 -0400

    removed unncessary code

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -2,10 +2,6 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import Navbar from "@/components/Navbar";
-import Leaderboard from "@/components/Leaderboard";
-import Hackathons from "@/components/Hackathons";
-import { Html5QrcodeResult, Html5QrcodeScanner } from "html5-qrcode";
-import { useState, useEffect } from "react";
 import  Scanner  from "@/components/Scanner";
 
 export default function Qr() {
